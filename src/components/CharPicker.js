1651902bac9be3e922d5dc4896d3cc23d96672b6
Kenan Berker <kenan.berker@ocado.com>
2020-04-07 17:07:43 +0100

    Not working

--- src/components/CharPicker.js
+++ src/components/CharPicker.js
@@ -1,25 +1,21 @@
-import React, { useState, useEffect } from 'react';
+import React from 'react';
 import { useHTTP } from '../hooks/http';
 
 import './CharPicker.css';
 
 const CharPicker = props => {
   const [isLoading, fetchedData] = useHTTP('https://swapi.co/api/people', []);
-  const selectedCharacters = fetchedData.results.slice(0, 5);
-
-  setIsLoading(false);
-  setLoadedChars(
-    selectedCharacters.map((char, index) => ({
+  const selectedCharacters = fetchedData ? fetchedData.results.slice(0, 5).map((char, index) => ({
     name: char.name,
     id: index + 1
-  })))
+  })) : [];
 
     let content = <p>Loading characters...</p>;
 
     if (
       !isLoading &&
-      loadedChars &&
-      loadedChars.length > 0
+      selectedCharacters &&
+      selectedCharacters.length > 0
     ) {
       content = (
         <select
@@ -27,7 +23,7 @@ const CharPicker = props => {
           value={props.selectedChar}
           className={props.side}
         >
-          {loadedChars.map(char => (
+          {selectedCharacters.map(char => (
             <option key={char.id} value={char.id}>
               {char.name}
             </option>
@@ -36,7 +32,7 @@ const CharPicker = props => {
       );
     } else if (
       !isLoading &&
-      (!loadedChars || loadedChars.length === 0)
+      (!selectedCharacters || selectedCharacters.length === 0)
     ) {
       content = <p>Could not fetch any data.</p>;
     }
